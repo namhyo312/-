45113ac44dc98181977b4328865e3325d0fc8bd5
namhyo312 <42093263+namhyo312@users.noreply.github.com>
2019-10-19 21:47:58 +0900

    최종

--- Lecture Evaluation/src/util/Gmail.java
+++ Lecture Evaluation/src/util/Gmail.java
@@ -1,32 +1,22 @@
 package util;
 
-
-
 import javax.mail.Authenticator;
-
 import javax.mail.PasswordAuthentication;
 
-
-
 public class Gmail extends Authenticator {
-
-
-
     @Override
-
     protected PasswordAuthentication getPasswordAuthentication() {
-
-        return new PasswordAuthentication("boysky1234@naver.com","!skdi312k");
+       return new PasswordAuthentication("namhyo312@gmail.com","!skdi312k");
 
     }
 }
 
-    
 
 
 /*
- * PasswordAuthentication pa; public Gmail() { String mail_id =
- * "namhyo312@gmail.com"; String mail_pw = "!skdi312k";
+ * 
+ * public Gmail() { String mail_id = "namhyo312@gmail.com"; String mail_pw =
+ * "!skdi312k";
  * 
  * pa = new PasswordAuthentication(mail_id, mail_pw); }
  * 
